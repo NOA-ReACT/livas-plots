f633429cec98583d21c7a78f8b7d11c7cc5ef8d3
Thanasis Georgiou <ageorgiou@noa.gr>
2023-05-11 15:11:34 +0300

    Add cloud-free versions of some plots for per-orbit

--- livas_plots/per_orbit.py
+++ livas_plots/per_orbit.py
@@ -25,6 +25,7 @@ def aerosol_subtype_variable(nc):
 
 
 PER_ORBIT_PLOTS = [
+    ## AVD-related arrays
     Plot(
         title="Feature Type",
         variable="CALIPSO_Flags_and_Auxiliary/Flags/AVD_Feature_Type",
@@ -46,6 +47,7 @@ PER_ORBIT_PLOTS = [
         },
         norm=colors.aerosol_subtype_norm,
     ),
+    ## Backscatter vs Smoothing
     Plot(
         title="Raw Total Backscatter 532 nm",
         variable="CALIPSO_Optical_Products/Total_Backscatter_Coefficient_532",
@@ -76,6 +78,7 @@ PER_ORBIT_PLOTS = [
         },
         norm=colors.backscatter_norm,
     ),
+    ## Depol vs smoothing
     Plot(
         title="Particulate Depolarization Ratio 532 nm",
         variable=lambda nc: copy_filter(
@@ -108,6 +111,91 @@ PER_ORBIT_PLOTS = [
         },
         norm=colors.depol_norm,
     ),
+    # Cloud free backscatter vs smoothing
+    Plot(
+        title="Raw Total Backscatter 532 nm (Cloud-free)",
+        variable=lambda nc: copy_filter(
+            nc,
+            source_var="LIVAS/Cloud_Free/Pure_Dust_and_Fine_Coarse/Optical_Products/Pure_Dust_Backscatter_Coefficient_532",
+            target_var="CALIPSO_Optical_Products/Total_Backscatter_Coefficient_532",
+        ),
+        colorbar=colors.backscatter_cbar,
+        colorbar_kwargs={
+            "ticks": colors.backscatter_bins,
+            "format": FuncFormatter(colors.backscatter_cbar_tick_formatter),
+        },
+        norm=colors.backscatter_norm,
+    ),
+    Plot(
+        title="Total Backscatter 532 nm, smoothed ±1 bin (Cloud-free)",
+        variable=lambda nc: copy_filter(
+            nc,
+            source_var="LIVAS/Cloud_Free/Pure_Dust_and_Fine_Coarse/Optical_Products/Pure_Dust_Backscatter_Coefficient_532",
+            target_var="LIVAS/CALIPSO_Optical_Products_Smoothed/Altitudinally_Smoothed_by_180m/Backscatter_Coefficient_532",
+        ),
+        colorbar=colors.backscatter_cbar,
+        colorbar_kwargs={
+            "ticks": colors.backscatter_bins,
+            "format": FuncFormatter(colors.backscatter_cbar_tick_formatter),
+        },
+        norm=colors.backscatter_norm,
+    ),
+    Plot(
+        title="Total Backscatter 532 nm, smoothed ±2 bins (Cloud-free)",
+        variable=lambda nc: copy_filter(
+            nc,
+            source_var="LIVAS/Cloud_Free/Pure_Dust_and_Fine_Coarse/Optical_Products/Pure_Dust_Backscatter_Coefficient_532",
+            target_var="LIVAS/CALIPSO_Optical_Products_Smoothed/Altitudinally_Smoothed_by_420m/Backscatter_Coefficient_532",
+        ),
+        colorbar=colors.backscatter_cbar,
+        colorbar_kwargs={
+            "ticks": colors.backscatter_bins,
+            "format": FuncFormatter(colors.backscatter_cbar_tick_formatter),
+        },
+        norm=colors.backscatter_norm,
+    ),
+    # Cloud free backscatter vs smoothing
+    Plot(
+        title="Particulate Depolarization Ratio 532 nm (Cloud-free)",
+        variable=lambda nc: copy_filter(
+            nc,
+            source_var="LIVAS/Cloud_Free/Pure_Dust_and_Fine_Coarse/Optical_Products/Pure_Dust_Backscatter_Coefficient_532",
+            target_var="CALIPSO_Optical_Products/Particulate_Depolarization_Ratio_Profile_532",
+        ),
+        # variable="CALIPSO_Optical_Products/Particulate_Depolarization_Ratio_Profile_532",
+        colorbar=colors.depol_cbar,
+        colorbar_kwargs={
+            "ticks": colors.depol_bins,
+        },
+        norm=colors.depol_norm,
+    ),
+    Plot(
+        title="Particulate Depolarization Ratio 532 nm, smoothed ±1 bin (Cloud-free)",
+        variable=lambda nc: copy_filter(
+            nc,
+            source_var="LIVAS/Cloud_Free/Pure_Dust_and_Fine_Coarse/Optical_Products/Pure_Dust_Backscatter_Coefficient_532",
+            target_var="LIVAS/CALIPSO_Optical_Products_Smoothed/Altitudinally_Smoothed_by_180m/Particulate_Depolarization_Ratio_532",
+        ),
+        colorbar=colors.depol_cbar,
+        colorbar_kwargs={
+            "ticks": colors.depol_bins,
+        },
+        norm=colors.depol_norm,
+    ),
+    Plot(
+        title="Particulate Depolarization Ratio 532 nm, smoothed ±2 bin (Cloud-free)",
+        variable=lambda nc: copy_filter(
+            nc,
+            source_var="LIVAS/Cloud_Free/Pure_Dust_and_Fine_Coarse/Optical_Products/Pure_Dust_Backscatter_Coefficient_532",
+            target_var="LIVAS/CALIPSO_Optical_Products_Smoothed/Altitudinally_Smoothed_by_420m/Particulate_Depolarization_Ratio_532",
+        ),
+        colorbar=colors.depol_cbar,
+        colorbar_kwargs={
+            "ticks": colors.depol_bins,
+        },
+        norm=colors.depol_norm,
+    ),
+    ## Pure dust, fine/coarse mode arrays
     Plot(
         title="Pure Dust Backscatter 532 nm",
         variable="LIVAS/Cloud_Free/Pure_Dust_and_Fine_Coarse/Optical_Products/Pure_Dust_Backscatter_Coefficient_532",
